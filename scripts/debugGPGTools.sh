f285b0753f86b2d02d5b4ce85af3b6e3c8fa85b3
Alexander Willner <alex@willner.ws>
2011-04-10 00:11:26 +0200

    no need for the secret key anymore and and added more information

--- scripts/debugGPGTools.sh
+++ scripts/debugGPGTools.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-if [ "$1" == "" ]; then
-  echo "Usage: $0 <YOURKEY>";
-  exit 1;
-fi
-
 :> $0.log
 exec 3>&1 4>&2 >$0.log 2>&1
 
 echo "*** Setup...";
-YOURKEY="$1"
-echo "  * $YOURKEY";
+YOURKEY="`grep ^default-key ~/.gnupg/gpg.conf|awk '{print $2}'`"
+echo "  * Default key: $YOURKEY";
 
 echo "*** Showing installed binaries...";
 echo "  * GPG1:"
@@ -21,16 +16,28 @@ which gpg2; gpg2 --version
 echo "*** Testing configuration...";
 gpg2 --gpgconf-test; echo $?
 
-echo "*** Testing encryption...";
+echo "*** The secret keys:";
 echo "  * GPG1:"
-echo "test"|gpg -aer "$YOURKEY"|gpg
+gpg -K
 echo "  * GPG2:"
-echo "test"|gpg2 -aer "$YOURKEY"|gpg2
+gpg2 -K
+
+echo "*** Testing encryption (1/2)...";
+if [ ! ""  == "$YOURKEY" ]; then
+  echo "  * GPG1:"
+  echo "test"|gpg -aer "$YOURKEY"|gpg
+  echo "  * GPG2:"
+  echo "test"|gpg2 -aer "$YOURKEY"|gpg2
+fi
+
+echo "*** Testing encryption (2/2)...";
+echo "  * GPG1:"
+echo "test"|gpg -ae --default-recipient-self|gpg
+echo "  * GPG2:"
+echo "test"|gpg2 -ae --default-recipient-self|gpg2
 echo "test"|gpg2 --default-recipient-self -ae
 echo "test"|gpg2 -as
 echo "test"|gpg2 --default-recipient-self -aes
-echo "  * The secret keys:";
-gpg2 -K
 
 echo "*** Showing installed bundles...";
 echo "  * /L/M/B:"
@@ -43,6 +50,9 @@ echo "  * Bundles enabled: ";
 defaults read com.apple.mail EnableBundles
 echo "  * Bundles compatibility: ";
 defaults read com.apple.mail BundleCompatibilityVersion
+
+echo "*** Configuration...";
+cat ~/.gnupg/gpg.conf
 
 echo "*** Some debugging information...";
 defaults write org.gpgtools.gpgmail GPGMailDebug -int 1
