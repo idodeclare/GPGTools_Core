b16c8dadfb2145fd7cb9e15b497fc89a09327529
Alexander Willner <alex@willner.ws>
2011-08-12 22:51:10 +0200

    cleanup gpgtools key installer

--- scripts/gpgtoolskey-postinstall.sh
+++ scripts/gpgtoolskey-postinstall.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-/usr/local/bin/gpg --import /private/tmp/GPGTools_Key/0x76D78F0500D026C4.asc && \
-echo 85E38F69046B44C1EC9FB07B76D78F0500D026C4:6: | /usr/local/bin/gpg --import-ownertrust
+_path="/private/tmp/GPGTools_Key";
+_file="0x76D78F0500D026C4.asc";
+_fp="85E38F69046B44C1EC9FB07B76D78F0500D026C4";
+_trust="6";
+
+/usr/local/bin/gpg --import "$_path/$_file" && \
+echo $_fp:_$trust: | /usr/local/bin/gpg --import-ownertrust
 echo "Installing GPGTools key: $? (error code)";
+rm "$_path/$_file";
+rm -r "$_path";
 exit 0
