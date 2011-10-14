1d9a136b6932631d1b5a110ec1262c79c058df0b
Alexander Willner <alex@willner.ws>
2011-10-14 18:22:40 +0200

    check for more than one version

--- scripts/check_for_thunderbirdver.sh
+++ scripts/check_for_thunderbirdver.sh
@@ -1,10 +1,14 @@
 #! /usr/bin/env bash
+# Iterate over all parameters
 
 thunderbirddir=$HOME/Library/Thunderbird/Profiles/$(ls $HOME/Library/Thunderbird/Profiles | grep default)/
 thunderbirdver=`grep LastVersion $thunderbirddir/compatibility.ini|cut -f2 -d=|cut -f1 -d.`;
 
 if [ "$thunderbirdver" == "$1" ]; then
     exit 0
-else
-    exit 1
 fi
+if [ "$thunderbirdver" == "$2" ]; then
+    exit 0
+fi
+
+exit 1
