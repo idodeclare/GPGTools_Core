ab0248ceb80f06c060b399ac95775855e4a8b4eb
Alexander Willner <alex@willner.ws>
2011-10-21 13:42:20 +0200

    also upload signature and add hash

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -21,6 +21,9 @@ if [ -z "$version" ]; then
 fi
 dmgName=${dmgName:-"$name-$version.dmg"}
 dmgPath=${dmgPath:-"build/$dmgName"}
+dmgHash="`shasum $dmgPath|cut -d " " -f 1`"
+sigPath="$dmgPath.sig"
+sigHash="`shasum $sigPath|cut -d " " -f 1`"
 #-------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------
@@ -31,11 +34,17 @@ fi
 
 if [ "$dmgPath" == "" -o "$dmgUrl" == "" -o "$version" == "" -o ! -e "$dmgPath" ] ; then
   echo "ERROR: config not complete" >&2
+  echo " * sigPath: $sigPath" >&2
   echo " * dmgPath: $dmgPath" >&2
   echo " * dmgUrl: $dmgUrl" >&2
+  echo " * dmgHash: $dmgHash" >&2
+  echo " * sigHash: $sigHash" >&2
   echo " * version: $version" >&2
   exit 2
 fi
 
-ruby ./Dependencies/GPGTools_Core/scripts/github_upload.rb "$dmgPath" "$dmgUrl"
+ruby ./Dependencies/GPGTools_Core/scripts/github_upload.rb "$dmgPath" "$dmgUrl" "SHA1 = $dmgHash"
+if [ -e "$sigPath" ]; then
+    ruby ./Dependencies/GPGTools_Core/scripts/github_upload.rb "$sigPath" "$dmgUrl" "SHA1 = $sigHash"
+fi
 #-------------------------------------------------------------------------
