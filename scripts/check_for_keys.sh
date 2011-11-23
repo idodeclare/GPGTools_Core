b1cc81f917fafac5af51a966eea09cb7d52bd445
Alexander Willner <alex@willner.ws>
2011-11-23 16:41:21 +0100

    small fixes

--- scripts/check_for_keys.sh
+++ scripts/check_for_keys.sh
@@ -3,11 +3,10 @@
 # This skript runs GKA if no private key was found.
 #
 
-keys="`gpg --list-secret-keys 2>/dev/null`";
+keys="`gpg --homedir=$USER/.gnupg  --list-secret-keys 2>/dev/null`";
 
 if [ "$keys" == "" ]; then
   open -a "GPG Keychain Access" --args --gen-key
-  exit 1;
 fi
 
 exit 0;
