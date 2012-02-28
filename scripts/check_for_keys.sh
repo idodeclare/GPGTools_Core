31f1aa9cb0e02ac735c5da9c8825b4539d567fd4
Alex <alex@willner.ws>
2012-02-28 20:29:30 +0100

    make installer more robust

--- scripts/check_for_keys.sh
+++ scripts/check_for_keys.sh
@@ -3,35 +3,38 @@
 # This skript runs GKA if no private key was found.
 #
 
-echo "Setup environment..."
-[ -r "$HOME/.profile" ] && . "$HOME/.profile"
-PATH=$PATH:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/local/bin
+echo "[GCK] Setup environment..."
+
+#This happen to break the installer on some systems
+#[ -r "$HOME/.profile" ] && . "$HOME/.profile"
+
+PATH=$PATH:/usr/local/MacGPG2/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/local/bin
 
 echo "Test for gpg..."
 if [ "`which gpg`" == "" ]; then
-  echo "[Keys] No GPG found"
+  echo "[GCK] No GPG found"
   exit 0;
 fi
 
-echo "Test for GKA..."
+echo "[GCK] Test for GKA..."
 if [ ! -e "/Applications/GPG Keychain Access.app" ]; then
-  echo "[Keys] No GKA"
+  echo "[GCK] No GKA"
   exit 0;
 fi
 
-echo "Test for keys..."
+echo "[GCK] Test for keys..."
 keys="`su $USER -c 'gpg --homedir=$HOME/.gnupg -K 2>/dev/null'`";
 
-echo "Open GKA..."
+echo "[GCK] Open GKA..."
 if [ "$keys" == "" ]; then
   #su - $USER -c "/Applications/GPG\ Keychain\ Access.app/Contents/MacOS/GPG\ Keychain\ Access --gen-key" &
   su - $USER -c "open /Applications/GPG\ Keychain\ Access.app --args --gen-key"
   #Again for 10.5
   su - $USER -c "open /Applications/GPG\ Keychain\ Access.app"
-  echo "Open First Steps page..."
+  echo "[GCK] Open First Steps page..."
   open http://support.gpgtools.org/kb/how-to/first-steps-where-do-i-start-where-do-i-begin
 else
-  echo "[Keys] Sec-Key found"
+  echo "[GCK] Sec-Key found"
 fi
 
 exit 0;
