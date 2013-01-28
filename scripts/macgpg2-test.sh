ac5f1bda7f41cac3b9099136fcff05b8b7d49c2b
AlexanderWillner <alexander.willner@tu-berlin.de>
2013-01-28 08:18:05 +0100

    fixing path

--- scripts/macgpg2-test.sh
+++ scripts/macgpg2-test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 _log="/tmp/`basename $0`.log"
+_path="/usr/local/MacGPG2/bin"
 
 echo -n "[Files installed] "
-[ -d /usr/local/MacGPG2 ] && echo "OK" || (echo "ERROR"; exit 1)
+[ -d "${_path}" ] && echo "OK" || (echo "ERROR"; exit 1)
 
 echo -n "[Executable] "
-gpg --version > "${_log}" 2>&1 && echo "OK" || (echo "ERROR:"; cat "${_log}"; exit 1)
+"${_path}"/gpg2 --version > "${_log}" 2>&1 && echo "OK" || (echo "ERROR:"; cat "${_log}"; exit 1)
