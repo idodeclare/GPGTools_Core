70a4df166f22e81aa9e4f249daf699e82679d188
AlexanderWillner <alexander.willner@tu-berlin.de>
2013-01-28 08:10:39 +0100

    show error by default

--- scripts/macgpg2-test.sh
+++ scripts/macgpg2-test.sh
@@ -3,7 +3,7 @@
 _log="/tmp/`basename $0`.log"
 
 echo -n "[Files installed] "
-[ -d /usr/local/MacGPG2 ] && echo "OK" || (echo "ERR"; exit 1)
+[ -d /usr/local/MacGPG2 ] && echo "OK" || (echo "ERROR"; exit 1)
 
 echo -n "[Executable] "
-gpg --version > "${_log}" 2>&1 && echo "OK" || (echo "ERR. See ${_log}"; exit 1)
+gpg --version > "${_log}" 2>&1 && echo "OK" || (echo "ERROR:"; cat "${_log}"; exit 1)
