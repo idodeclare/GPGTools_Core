dcdeec05557c94318866ef7d8ee91fa7d39b21bc
Mento <Mento@gpgtools.org>
2013-02-01 12:28:58 +0100

    some other improvements

--- scripts/gpgtools-autofix.sh
+++ scripts/gpgtools-autofix.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
-########################################
+################################################################################
 # Cleanup and fixes for GnuPG on OS X.
 #
-# @author	Alex (alex@gpgtools.org)
+# @author	Alex (alex@gpgtools.org) and Mento (mento@gpgtools.org)
 # @see		http://gpgtools.org
 # @todo		Invoke this script from other scripts
-########################################
+################################################################################
 
-function errExit() {
+
+# Helper functions #############################################################
+function errReturn {
+	msg="$* (${BASH_SOURCE[1]##*/}: line ${BASH_LINENO[0]})"
+	if [[ -t 1 ]] ;then
+		echo -e "\033[1;31m$msg\033[0m" >&2
+	else
+		echo "$msg" >&2
+	fi
+	return 1
+}
+function errExit {
 	msg="$* (${BASH_SOURCE[1]##*/}: line ${BASH_LINENO[0]})"
 	if [[ -t 1 ]] ;then
 		echo -e "\033[1;31m$msg\033[0m" >&2
@@ -16,34 +27,28 @@ function errExit() {
 	fi
 	exit 1
 }
-function echoBold() {
-	if [[ -t 1 ]] ;then
-		echo -e "\033[1m$*\033[0m"
-	else
-		echo -e "$*"
-	fi
+function myEcho {
+	echo "${SCRIPT_NAME:+[$SCRIPT_NAME] }$*"
 }
-function myChown() {
+function myChown {
 	# chown -R "$USER", if the file exists.
     [[ -e "$1" ]] && sudo chown -R "$USER" "$1"
 }
-function myChmod() {
+function myChmod {
 	# chmod, if the file exists.
 	eval [[ -e "\${$#}" ]] && sudo chmod "$@"
 }
-
-
-USER=${USER:-$(id -un)}
+################################################################################
 
 
 function fixGPGToolsPreferences {
-    echo "[gpgtools] Fixing Preferences..."
+    myEcho "Fixing Preferences..."
     gpgp_dir="$HOME/Library/PreferencePanes"
 	myChown "$gpgp_dir"
 }
 
 function fixGPGServices {
-    echo "[gpgtools] Fixing Services..."
+    myEcho "Fixing Services..."
     gpgs_dir="$HOME/Library/Services/GPGServices.service"
     myChown "$gpgs_dir"
     [[ -e "/private/tmp/ServicesRestart" ]] && sudo /private/tmp/ServicesRestart
@@ -51,79 +56,95 @@ function fixGPGServices {
 }
 
 function updateGPGMail {
-    # config ###################################################################
+    # config
     sysdir="/Library/Mail/Bundles"
     netdir="/Network/Library/Mail/Bundles"
     homedir="$HOME/Library/Mail/Bundles"
     bundle="GPGMail.mailbundle"
-    ############################################################################
 
-    # modify the defaults ######################################################
+    # modify the defaults 
     defaults write org.gpgtools.gpgmail DecryptMessagesAutomatically -bool YES
     defaults write com.apple.mail DecryptMessagesAutomatically -bool YES
-    ############################################################################
 
-    # determine the bundle is located ##########################################
-    if [[ -e "$netdir/$bundle" ]] ;then
-        _target="$netdir"
-    elif [[ -e "$sysdir/$bundle" ]] ;then
-        _target="$sysdir"
-    elif [[ -e "$homedir/$bundle" ]] ;then
-         _target="$homedir"
+	# Find GPGMail
+	normalDirs=("$sysdir" "$netdir" "$homedir")
+	disabledDirs=("$sysdir ("* "$netdir ("* "$homedir ("*)
+	found=""
+
+	for dir in "${normalDirs[@]}" ;do
+		bundlePath="$dir/$bundle"
+		if [[ -d "$bundlePath" ]] ;then
+			if [[ -z "$found" ]] ;then
+				myEcho "Found GPGMail at '$bundlePath'"
+				found=$bundlePath
+			else
+				myEcho "Deleting duplicate bundle '$bundlePath'."
+				rm -rf "$bundlePath"
+			fi
+		fi
+	done
+	
+	if [[ -z "$found" ]] ;then
+		for dir in "${disabledDirs[@]}" ;do
+			bundlePath="$dir/$bundle"
+			if [[ -d "$bundlePath" ]] ;then
+				myEcho "Found disabled GPGMail at '$bundlePath', moving it..."
+				newDir="${dir%/*}/Bundles"
+				found="$newDir/$bundle"
+				mkdir -p "$newDir"
+				mv "$bundlePath" "$newDir"
+				myEcho "GPGMail was reinstalled."
+				break;
+			fi
+		done
+		if [[ -z "$found" ]] ;then
+			myEcho "GPGMail not found"
+			return 1
+		fi
+	fi
+	bundlePath=$found
+
+	# Setting permissions
+    myEcho "Setting the correct permissions in '$bundlePath'..."
+    myChmod -R u+rwX,go=rX "$bundlePath"
+
+	# Fix UUIDs
+	updateGPGMailUUIDs "$bundlePath"
+}
+
+function updateGPGMailUUIDs {
+	# Usage: updateGPGMailUUIDs /Library/Mail/Bundles/GPGMail.mailbundle
+	# The var PlistBuddy need to be set, to the path to PlistBuddy.
+	plistBundle="$1/Contents/Info.plist"
+	plistMail="/Applications/Mail.app/Contents/Info.plist"
+    plistFramework="/System/Library/Frameworks/Message.framework/Resources/Info.plist"
+	entry="SupportedPluginCompatibilityUUIDs"
+
+	myEcho "Patching GPGMail UUIDs..."
+
+	uuid1=$("$PlistBuddy" -c "print PluginCompatibilityUUID" "$plistMail")
+	uuid2=$("$PlistBuddy" -c "print PluginCompatibilityUUID" "$plistFramework")
+	
+    [[ -n "$uuid1" && -n "$uuid2" ]] ||
+		errReturn "Warning: could not patch GPGMail. No UUIDs found."
+    [[ -f "$plistBundle" ]] ||
+        errReturn "Warning: could not patch GPGMail. No bundle found."
+
+
+	content=$("$PlistBuddy" -c "print $entry" "$plistBundle")
+
+    if grep -q "$uuid1" <<<"$content" && grep -q "$uuid2" <<<"$content" ;then
+        myEcho "GPGMail already patched"
     else
-        return 0
-    fi
-    ############################################################################
-
-    ############################################################################
-    _bundleId="gpgmail"
-    _bundleName="$bundle"
-    _bundleRootPath="$_target"
-    _bundlePath="$_bundleRootPath/$_bundleName"
-    _plistBundle="$_bundlePath/Contents/Info"
-    _plistMail="/Applications/Mail.app/Contents/Info"
-    _plistFramework="/System/Library/Frameworks/Message.framework/Resources/Info"
-
-    if [[ -d "$_bundlePath" ]] ;then
-        echo "[$_bundleId] is installed"
-    else
-        foundDisabled=$(find "$_bundleRootPath ("* -type d -name "$_bundleName" 2>/dev/null|head -n1)
-        if [[ "" != "$foundDisabled" ]] ;then
-            mkdir -p "$_bundleRootPath"
-            mv "$foundDisabled" "$_bundleRootPath"
-           echo "[$_bundleId] was reinstalled"
-        else
-            echo "[$_bundleId] not found"
-        fi
-    fi
-    
-    echo "[$_bundleId] Setting the correct permissions in '$_bundlePath'..."
-    myChmod -R u+rwX,go=rX "$_bundlePath"
-
-    uuid1=$(defaults read "$_plistMail" "PluginCompatibilityUUID")
-    uuid2=$(defaults read "$_plistFramework" "PluginCompatibilityUUID")
-    if [[ "" == "$uuid1" ]] || [[ "" == "$uuid2" ]]  ;then
-        echo "[$_bundleId] Warning: could not patch GPGMail. No UUIDs found."
-        return
-    fi
-    if [[ ! -f "$_plistBundle.plist" ]] ;then
-        echo "[$_bundleId] Warning: could not patch GPGMail. No bundle found."
-        return
-    fi
-    isPatched1=$(grep $uuid1 "$_bundlePath/Contents/Info.plist" 2>/dev/null)
-    isPatched2=$(grep $uuid2 "$_bundlePath/Contents/Info.plist" 2>/dev/null)
-    if [[ "" != "$isPatched1" ]] && [[ "" != "$isPatched2" ]]  ;then
-        echo "[$_bundleId] already patched"
-    else
-        defaults write "$_plistBundle" "SupportedPluginCompatibilityUUIDs" -array-add "$uuid1"
-        defaults write "$_plistBundle" "SupportedPluginCompatibilityUUIDs" -array-add "$uuid2"
-        plutil -convert xml1 "$_plistBundle.plist"
-        echo "[$_bundleId] successfully patched"
+		"$PlistBuddy" -c "add $entry:0 string $uuid1" -c "add $entry:0 string $uuid2" "$plistBundle"
+        myEcho "GPGMail successfully patched"
     fi
 }
 
+
+
 function fixGPGMail {
-    echo "[gpgtools] Fixing Mail..."
+    myEcho "Fixing Mail..."
 	
 	domain="com.apple.mail"
 	case "$(sw_vers -productVersion | cut -d . -f 2)" in
@@ -132,11 +153,11 @@ function fixGPGMail {
 	    *) bundleCompVer=3 ;; 
     esac
 	
-	echo " * Writing '$bundleCompVer' to '$domain'..."
+	myEcho " * Writing '$bundleCompVer' to '$domain'..."
 	defaults write "$domain" EnableBundles -bool YES
 	defaults write "$domain" BundleCompatibilityVersion -int $bundleCompVer
 	
-	echo " * Writing '$bundleCompVer' to '$domain' as '$USER'..."
+	myEcho " * Writing '$bundleCompVer' to '$domain' as '$USER'..."
 	sudo -u  "$USER" defaults write "$domain" EnableBundles -bool YES
 	sudo -u  "$USER" defaults write "$domain" BundleCompatibilityVersion -int $bundleCompVer
 
@@ -144,21 +165,19 @@ function fixGPGMail {
     if [[ "$(whoami)" == root ]]  ;then
 	    #defaults acts funky when asked to write to the root domain but seems to work with a full path
 		domain=/Library/Preferences/com.apple.mail
-	fi	
-
-	echo " * Writing '$bundleCompVer' to '$domain'..."
-	defaults write "$domain" EnableBundles -bool YES
-	defaults write "$domain" BundleCompatibilityVersion -int $bundleCompVer
-
+		myEcho " * Writing '$bundleCompVer' to '$domain'..."
+		defaults write "$domain" EnableBundles -bool YES
+		defaults write "$domain" BundleCompatibilityVersion -int $bundleCompVer
+	fi
 
 
     gpgm_dir="$HOME/Library/Mail/"
-	echo " * Fixing permissions in '$gpgm_dir'..."
+	myEcho " * Fixing permissions in '$gpgm_dir'..."
     [[ -e "$gpgm_dir" ]] && sudo chown "$USER" "$gpgm_dir"
     myChown "$gpgm_dir/Bundles"
 
     gpgm_dir="/Library/Mail/"
-	echo " * Fixing permissions in '$gpgm_dir'..."
+	myEcho " * Fixing permissions in '$gpgm_dir'..."
     myChmod -R u+rwX,go=rX "$gpgm_dir"
     
     updateGPGMail
@@ -201,7 +220,7 @@ function findWorkingPinentry {
 
 
 function fixGPG {
-    echo "[gpgtools] Fixing GPG..."
+    myEcho "Fixing GPG..."
     fixGnupgHomePermissions
 
     sudo mkdir -p /usr/local/bin
@@ -216,12 +235,12 @@ function fixGPG {
 		# Create a new gpg.conf if none is existing from the skeleton file
 		if [[ ! -e "$GNUPGHOME/gpg.conf" ]] ;then
 			cp "/usr/local/MacGPG2/share/gnupg/gpg-conf.skel" "$GNUPGHOME/gpg.conf"
-			echo "[MacGPG2] Created gpg.conf"    
+			myEcho "Created gpg.conf"
 		elif ! /usr/local/MacGPG2/bin/gpg2 --gpgconf-test ;then
-			echo "Fixing gpg.conf"
+			myEcho "Fixing gpg.conf"
 			mv "$GNUPGHOME/gpg.conf" "$GNUPGHOME/gpg.conf.moved-by-gpgtools-installer"
 			cp /usr/local/MacGPG2/share/gnupg/gpg-conf.skel "$GNUPGHOME/gpg.conf"
-			echo "[MacGPG2] Replaced gpg.conf"    
+			myEcho "Replaced gpg.conf"
 		fi
 	fi
 
@@ -234,7 +253,7 @@ function fixGPG {
 }
 
 
-function fixGPGAgent() {
+function fixGPGAgent {
 	gpgAgentConf="$GNUPGHOME/gpg-agent.conf"
 	touch "$gpgAgentConf"
 
@@ -242,18 +261,18 @@ function fixGPGAgent() {
 	currentPinetry=$(sed -En '/^[ 	]*pinentry-program "?([^"]*)"?/{s//\1/p;q;}' "$gpgAgentConf")
 	if ! isPinentryWorking "$currentPinetry" ;then
 		# Let's find a working pinentry
-		echo "Found working pinentry at: $working_pinentry"
+		myEcho "Found working pinentry at: $working_pinentry"
 		if ! working_pinentry=$(findWorkingPinentry) ;then
-			echo "No working pinentry found. Abort!"
+			myEcho "No working pinentry found. Abort!"
 			return 1
 		fi
 
 		if [[ -n "$currentPinetry" ]] ;then
-			echo "Replacing existing pinentry"
+			myEcho "Replacing existing pinentry"
 			sed -Ei '' '/^([ 	]*pinentry-program ).*/s@@\1'"$working_pinentry@" "$gpgAgentConf"
 		else
-			echo "Add new pinentry"
-			echo -e "pinentry-program $working_pinentry" >> "$gpgAgentConf"
+			myEcho "Add new pinentry"
+			echo "pinentry-program $working_pinentry" >> "$gpgAgentConf"
 		fi
 	fi
 
@@ -273,7 +292,7 @@ function fixGPGAgent() {
     rm -f "$GNUPGHOME/S.gpg-agent" "$GNUPGHOME/S.gpg-agent.ssh"
 }
 
-function generalFixes() {
+function generalFixes {
 	# Remove old plist files.
 	rm -f "$HOME/Library/LaunchAgents/org.gpgtools.macgpg2.gpg-agent.plist" \
 		"/Library/LaunchAgents/org.gpgtools.macgpg2.gpg-agent.plist" \
@@ -294,6 +313,11 @@ function generalFixes() {
 
 
 GNUPGHOME=${GNUPGHOME:-"$HOME/.gnupg"}
+PlistBuddy=/usr/libexec/PlistBuddy
+SCRIPT_NAME=autofix
+
+sudo echo >/dev/null || errExit "No root privileges! Aborting."
+
 
 generalFixes
 fixGPG
