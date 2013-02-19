6a9dac711c06e5795639109502d0460b657429dc
Lukas Pitschl <lukas@dressyvagabonds.com>
2013-02-19 01:00:52 +0100

    [FEATURE] Single script to prepare a new release
    - Find a release notes file or abort if not available.
    - Update the Makefile.config with the new version
    - Commit release notes and Makefile.config
    - Tag the new version
    - Merge the new version into master and push to start deployment
      on the buildserver.
    - Add make targets to easily start releasing via make.

--- newBuildSystem/prepare-release.py
+++ newBuildSystem/prepare-release.py
@@ -29,6 +29,10 @@ Following steps are executed in order to prepare a release:
 """
 
 import os, sys
+import fileinput
+import re
+import shlex
+
 from optparse import OptionParser, OptionValueError
 
 sys.path.insert(1, os.path.join(os.getcwd(), './Dependencies/GPGTools_Core/python'))
@@ -39,6 +43,11 @@ from clitools.color import *
 CWD = os.getcwd()
 VERSION_FILE = "Makefile.config"
 VERSION_PATH = "%s/%s" % (CWD, VERSION_FILE)
+RELEASE_NOTES_FOLDER = "Release Notes"
+RELEASE_NOTES_FOLDER_PATH = "%s/%s" % (CWD, RELEASE_NOTES_FOLDER)
+COMMIT_MSG = """Release of version: %s"""
+SOURCE_BRANCH="deploy-dev"
+DESTINATION_BRANCH="deploy-master"
 
 def check_version(version):
     """Check if the format of the version string represents a valid version.
@@ -176,14 +185,89 @@ def confirm_version(release_type):
     return new_version
 
 def is_workspace_clean():
+    """In order to release a new version, it's absolutely necessary that the
+    workspace doesn't contain any changes.
+    
+    The only files which are allowed to be new or modified during a commit, are
+    release notes, since the release script is responsible for adding them and checking
+    them in.
+    """
     out = run("git status --short").strip()
-    return out == ""
+    # Release Notes/ entries are allowed, even necessary.
+    lines = out.split("\n")
+    clean = True
+    for l in lines:
+        if l != "" and l.find("Release Notes") == -1:
+            clean = False
+    
+    return clean
+
+def find_release_notes(version):
+    """Checks if there's a release notes file available for the specified version.
+    
+    Optionally allows the developer to create one automatically.
+    """
+    release_notes_path = "%s/%s.json" % (RELEASE_NOTES_FOLDER_PATH, format_version(version))
+    
+    if not os.path.isfile(release_notes_path):
+        return None
+    
+    return release_notes_path
+
+def release_notes_file(version):
+    return "%s/%s.json" % (RELEASE_NOTES_FOLDER_PATH, format_version(version))
+
+def update_version(version):
+    """Write the new version to the Makefile.config file."""
+    version["revision"] = version["patch"]
+    
+    #fp = open(VERSION_PATH, "r")
+    for line in fileinput.FileInput(VERSION_PATH, inplace=True):
+        line = line.strip()
+        if line.find("=") == -1:
+            print line
+            continue
+            
+        (key, value) = line.split("=")
+        if key.lower() not in version.keys():
+            print line
+            continue
+        
+        line = re.sub(r'%s=([^\s]+)' % (key.upper()), '%s=%s' % (key.upper(), version[key.lower()]), line)
+        print line
+    
+def checkin_release_info_and_tag(version):
+    """Commit the release notes and the updated Makefile.config"""
+    try:
+        run('git add "%s"' % (VERSION_PATH))
+        run('git add "%s"' % release_notes_file(version))
+        run('git commit -m "%s"' % (COMMIT_MSG % (format_version(version))))
+        run('git tag -a v%s  -m "%s"' % (format_version(version), COMMIT_MSG % (format_version(version))))
+    except Exception, e:
+        error("Couldn't checkin updated version files.\n\n%s" % (e))
+        return False
+    
+    return True
+
+def merge_and_push(src, dst):
+    # Checkout the master branch.
+    run("git checkout %s" % (dst))
+    # Merge in the release info from the dev branch.
+    run("git merge %s" % (src))
+    # Push the master branch so the actual deployment of the release
+    # is triggered on the build server.
+    print("git push origin %s" % (dst))
+    # Checkout the dev branch again, so the developer can continue to develop. 
+    run("git checkout %s" % (src))
 
 def format_version(version):
     return "%s.%s.%s" % (str(version["major"]), str(version["minor"]), str(version["patch"]))
 
 def run(cmd):
-    return check_output(cmd.split(" "))
+    if type(cmd) == type(""):
+        cmd = shlex.split(cmd)
+    
+    return check_output(cmd)
 
 def main():
     (options, args) = parse_options()
@@ -202,10 +286,31 @@ def main():
     
     title("Prepare release for %s %s" % ("MacGPG", format_version(new_version)))
     
+    # Find the release notes. If none are available, ask the developer
+    # to create one for them.
+    status("Finding Release Notes")
     
-    print run("ls -l")
+    release_notes = find_release_notes(new_version)
+    if not release_notes:
+        error("""There are no release notes available - can't continue!\n"""
+              """Consider running `make release-notes version=%s` and try again.""" % (format_version(new_version)))
     
-    print "New version: %s" % (new_version)
+    status("  Found: %s/%s" % (RELEASE_NOTES_FOLDER, os.path.basename(release_notes)))
+    
+    # Release notes are available, not onto updating the version in the config
+    # file and checking everything in.
+    # Tag the version so it can be easily re-created.
+    status("Update version in Makefile.config")
+    update_version(new_version)
+    status("Create version tag: v%s" % (format_version(new_version)))
+    checkin_release_info_and_tag(new_version) 
+    
+    # Merge the changes into the master branch (or the one specified) and push it.
+    status("Pushing release to start deployment")
+    merge_and_push(SOURCE_BRANCH, DESTINATION_BRANCH)
+    
+    success("""The new release %s has been successfully prepared.\n"""
+            """Check http://build-ml.gpgtools.org to follow further progress.""" % (format_version(new_version)))
     
     return True
 
