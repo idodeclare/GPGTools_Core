aef121f6ca29eb8550fa50e66748b477d1087387
Mento <Mento@gpgtools.org>
2013-02-19 22:43:09 +0100

    Don't run uninstallerSig during every build.

--- newBuildSystem/uninstallerSig.sh
+++ newBuildSystem/uninstallerSig.sh
@@ -4,15 +4,11 @@ source "$(dirname "${BASH_SOURCE[0]}")/core.sh"
 parseConfig
 
 
-if [[ "$PKG_SIGN" == "1" ]]; then
-    echo "Checking the environment..."
-	[[ -d "$rmPath" ]] || errExit "I require app '$rmPath' but it does not exit. Aborting."
-    
-    echo "Signing '$rmPath'..."
-    codesign -s "${certNameApp}" -f "$rmPath" || errExit "Can't sign '$rmPath'. Aborting."
+echo "Checking the environment..."
+[[ -d "$rmPath" ]] || errExit "I require app '$rmPath' but it does not exit. Aborting."
 
-    echo "Validating '$rmPath' signature..."
-    codesign -v "$rmPath" || errExit "New signature of app '$rmPath' is invalid. Aborting."
-else
-	echo "uninstallerSig Warning: PKG_SIGN not enabled. Skipping."
-fi
+echo "Signing '$rmPath'..."
+codesign -s "${certNameApp}" -f "$rmPath" || errExit "Can't sign '$rmPath'. Aborting."
+
+echo "Validating '$rmPath' signature..."
+codesign -v "$rmPath" || errExit "New signature of app '$rmPath' is invalid. Aborting."
