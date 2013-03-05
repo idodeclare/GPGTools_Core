486be9c98dd64463a30fd30b9a159aa07980debb
Mento <Mento@gpgtools.org>
2013-03-05 23:14:19 +0100

    Add progress indicator

--- newBuildSystem/aws-upload.py
+++ newBuildSystem/aws-upload.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python -u
 
 # This script uploads a file to Amazon S3.
 # It uses the key specified in AWS_ACCESS_KEY and gets the secret from the Keychain.
@@ -10,13 +10,25 @@ BUCKET = 'gpgtools'
 import sys, os
 from subprocess import check_output, CalledProcessError
 
+
+
+def progress_callback(current, total):
+	percent = current * 100 / total
+	sys.stdout.write('\033[1K\r%i %%' % (percent))
+	if current == total:
+		sys.stdout.write('\n')
+
+
+
+# Main part.
 try:
     from boto.s3.connection import S3Connection
     from boto.exception import S3ResponseError
 except ImportError:
     sys.exit("Can't find boto! Use \"sudo easy_install boto\" to install it.")
 
-	
+
+
 if len(sys.argv) != 2:
 	exit('Usage: aws-upload.py file')
 
@@ -25,13 +37,11 @@ if not os.path.isfile(file):
 	exit('File not found "' + file + '"')
 
 filename = os.path.basename(file)
-
 try:
 	secret = check_output(["security", "find-generic-password", "-w", "-a", "AKIAIDCNEPBLS2IMO55A"])
 	secret = secret[:-1]
 except CalledProcessError:
 	exit("Can't get AWS secret access key! Do you have it in your keychain?")
-
 
 try:
 	connection = S3Connection(AWS_ACCESS_KEY, secret)
@@ -40,11 +50,15 @@ try:
 	# Check if there's no such file already on the server.
 	awsfile = bucket.get_key(filename)
 	if awsfile:
-	    exit("%s already exists on S3. Remove first!" % (filename))
+		exit("%s already exists on S3. Remove first!" % (filename))
 	
 	awsfile = bucket.new_key(filename)
+	if os.isatty(1):
+		print 'Upload "%s"...  ' % (filename)
+		awsfile.set_contents_from_filename(file, cb=progress_callback, num_cb=100)
+	else:
+		awsfile.set_contents_from_filename(file) # Only show the progress if stdout is a terminal.
 
-	awsfile.set_contents_from_filename(file)
 	awsfile.set_acl('public-read')
 except S3ResponseError, e:
 	if e.status == 403:
