3822f82176fd1eb71e0a0f80ea31e8e199561ba2
Lukas Pitschl <lukas@dressyvagabonds.com>
2013-03-26 22:12:03 +0100

    Use NO_COLORS environment var to determine if color codes should be used.

--- python/clitools/color.py
+++ python/clitools/color.py
@@ -1,55 +1,69 @@
-import sys
+import sys, os
 
 class TerminalColor(object):
     @classmethod
     def blue(cls):
+        if not os.isatty(1) or os.getenv("NO_COLORS") == "1":
+            return ""
         return TerminalColor.bold(34)
     
     @classmethod
     def white(cls):
+        if not os.isatty(1) or os.getenv("NO_COLORS") == "1":
+            return ""
         return TerminalColor.bold(39)
     
     @classmethod
     def red(cls):
+        if not os.isatty(1) or os.getenv("NO_COLORS") == "1":
+            return ""
         return TerminalColor.underline(31)
     
     @classmethod
     def yellow(cls):
+        if not os.isatty(1) or os.getenv("NO_COLORS") == "1":
+            return ""
         return TerminalColor.underline(33)
     
     @classmethod
     def reset(cls):
+        if not os.isatty(1) or os.getenv("NO_COLORS") == "1":
+            return ""
         return TerminalColor.escape(0)
     
     @classmethod
     def em(cls):
+        if not os.isatty(1) or os.getenv("NO_COLORS") == "1":
+            return ""
         return TerminalColor.underline(39)
     
     @classmethod
     def green(cls):
+        if not os.isatty(1) or os.getenv("NO_COLORS") == "1":
+            return ""
         return TerminalColor.color(92)
     
     @classmethod
     def color(cls, s):
-        if not sys.stdout.isatty():
+        if not os.isatty(1) or os.getenv("NO_COLORS") == "1":
             return s
         return TerminalColor.escape("0;%s" % (s))
     
     @classmethod
     def bold(cls, s):
-        if not sys.stdout.isatty():
+        if not os.isatty(1) or os.getenv("NO_COLORS") == "1":
             return s
         return TerminalColor.escape("1;%s" % (s))
     
     @classmethod
     def underline(cls, s):
-        if not sys.stdout.isatty():
+        if not os.isatty(1) or os.getenv("NO_COLORS") == "1":
             return s
         return TerminalColor.escape("4;%s" % (s))
     
     @classmethod
     def escape(cls, s):
-        if not sys.stdout.isatty():
+        if not os.isatty(1) or os.getenv("NO_COLORS") == "1":
             return s
         
         return "\033[%sm" % (s)
