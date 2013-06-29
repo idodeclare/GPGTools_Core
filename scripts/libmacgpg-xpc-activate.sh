2c83d9a9e6fd8fc1a9379dbe47dfe49bc4e5c1f2
Mento <Mento@gpgtools.org>
2013-06-29 15:00:24 +0200

    libmacgpg-xpc-activate.sh removed.

--- scripts/libmacgpg-xpc-activate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo -u "$USER" launchctl remove org.gpgtools.Libmacgpg.jailfree 2>/dev/null
-sudo -u "$USER" launchctl remove org.gpgtools.Libmacgpg.xpc 2>/dev/null
-sudo -u "$USER" launchctl load /Library/LaunchAgents/org.gpgtools.Libmacgpg.xpc.plist
-exit 0
